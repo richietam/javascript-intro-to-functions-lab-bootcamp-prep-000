4fc22aa6eb4bbd37eed801cd357599efaf4fd48f
richardtam.ny <richardtam.ny@gmail.com>
2018-11-21 16:40:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function shout(string) {
   return string.toUpperCase()
 }
 
+function whipser (string) {
+  return string.toLowerCase
+}
+
 
 function sayHiToGrandma(string) {
   return("I can't hear you!")
